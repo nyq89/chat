4707b11315570178ee25d981b06d2f6638a101fb
Robert Nyquist <nyquist@student.chalmers.se>
2016-07-09 14:18:30 +0200

    Explain test

--- test/private_message_test.exs
+++ test/private_message_test.exs
@@ -4,6 +4,8 @@ defmodule Chat.PrivateMessageTest do
   alias Chat.ChatServer
   alias Chat.ChatClient
 
+  #This is just for visual testeing.
+  #It will not show if the messages is correct or not.
   test "send private message" do
     server = ChatServer.start
     gashi = ChatClient.connect("gashi", server)
